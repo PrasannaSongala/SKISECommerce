0e23b73d77b7b468c0d652de774b289268bca2ff
PrasannaSongala <songalaprasanna@gmail.com>
2024-11-23 16:26:35 +0530

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
 // Set up the database connection
-const sequelize = new Sequelize('srikanya', 'root', 'Pass12!@', {
+const sequelize = new Sequelize('srikanya', 'root', 'Pass123!@#', {
   host: '127.0.0.1',
   dialect: 'mysql',
   logging: false, // Disable logging for production
