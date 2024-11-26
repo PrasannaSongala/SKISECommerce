5801d18f5930706b91b31d94077416197a55ac6e
PrasannaSongala <songalaprasanna@gmail.com>
2024-11-26 09:51:20 +0530

    Update swagger.ts

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -3,7 +3,7 @@ import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import { Express } from 'express';
 
-// Basic configuration for swagger-jsdoc
+
 const swaggerDefinition = {
   openapi: '3.0.0',
   info: {
@@ -21,25 +21,104 @@ const swaggerDefinition = {
       description: 'Local development server',
     },
   ],
+  components: {
+    schemas: {
+      Location: {
+        type: 'object',
+        required: ['name', 'address', 'city', 'state', 'pincode', 'latitude', 'longitude', 'isDefault'],
+        properties: {
+          id: {
+            type: 'integer',
+            description: 'Auto-generated ID',
+          },
+          name: {
+            type: 'string',
+            maxLength: 45,
+            description: 'Name of the location',
+          },
+          address: {
+            type: 'string',
+            maxLength: 150,
+            description: 'Address of the location',
+          },
+          city: {
+            type: 'string',
+            maxLength: 45,
+            description: 'City of the location',
+          },
+          state: {
+            type: 'string',
+            maxLength: 45,
+            description: 'State of the location',
+          },
+          pincode: {
+            type: 'integer',
+            description: 'Pincode of the location',
+          },
+          latitude: {
+            type: 'number',
+            format: 'float',
+            description: 'Latitude of the location',
+          },
+          longitude: {
+            type: 'number',
+            format: 'float',
+            description: 'Longitude of the location',
+          },
+          isDefault: {
+            type: 'boolean',
+            description: 'Whether the location is the default one',
+          },
+          createdAt: {
+            type: 'string',
+            format: 'date-time',
+            description: 'Timestamp when the location was created',
+          },
+          updatedAt: {
+            type: 'string',
+            format: 'date-time',
+            description: 'Timestamp when the location was last updated',
+          },
+        },
+      },
+    },
+  },
 };
 
 // Options for swagger-jsdoc
 const options = {
   definition: swaggerDefinition,
-  apis: ['./src/routes/brandlistRoutes.ts', 
-         './src/controllers/brandlistController.ts',
-        './src/routes/categoryDimensionRoutes.ts',
-      './src/controllers/categoryDimensionController.ts',
+  apis: [
+    './src/routes/brandlistRoutes.ts', 
+    './src/controllers/brandlistController.ts',
+    './src/routes/categoryDimensionRoutes.ts',
+    './src/controllers/categoryDimensionController.ts',
     './src/routes/categoryTypesRoutes.ts',
-  './src/controllers/categoryTypesController.ts'],
-         
+    './src/controllers/categoryTypesController.ts',
+    './src/routes/contactusRoutes.ts',
+    './src/controllers/contactusController.ts',
+    './src/routes/locationsRoutes.ts',
+    './src/controllers/locationsController.ts',
+    './src/routes/productRoutes.ts',
+    './src/controllers/productController.ts',
+    './src/routes/productDimensionRoutes.ts',
+    './src/controllers/productDimensionController.ts',
+    './src/routes/inventoryRoutes.ts',
+    './src/controllers/inventoryController.ts',
+    './src/routes/inventoryListRoutes.ts',
+    './src/controllers/inventoryListController.ts',
+    './src/routes/inventoryTransactionRoutes.ts',
+    './src/controllers/inventoryTransactionController.ts',
+    './src/routes/sellerRoutes.ts',
+    './src/controllers/sellerController.ts'
+  ],
 };
 
 const swaggerSpec = swaggerJSDoc(options);
 
 // Swagger UI setup function
 const setupSwagger = (app: Express) => {
-  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec)); // Swagger UI endpoint
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec)); 
   app.get('/api-docs.json', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.send(swaggerSpec);
