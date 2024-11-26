6f4d9fb5314f3c52e606c28d4679787826733b27
PrasannaSongala <songalaprasanna@gmail.com>
2024-11-26 09:59:58 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import express from 'express';
-import sequelize from './config/database'; // Ensure sequelize is correctly initialized
-import { setupSwagger } from './config/swagger'; // Swagger setup
-import brandlistRoutes from './routes/brandlistRoutes'; // Ensure the path is correct
-import categoryDimensionRoutes from './routes/categoryDimensionRoutes'; // Check if the path is correct
+import sequelize from './config/database'; 
+import { setupSwagger } from './config/swagger'; 
+import brandlistRoutes from './routes/brandlistRoutes'; 
+import categoryDimensionRoutes from './routes/categoryDimensionRoutes'; 
 import categoryTypesRoutes from './routes/categoryTypesRoutes'; 
+import contactusRoutes from './routes/contactusRoutes';
+import locationsRoutes from './routes/locationsRoutes'; 
+import productRoutes from './routes/productRoutes'; 
+import productDimensionRoutes from './routes/productDimensionRoutes';
+import inventoryRoutes from './routes/inventoryRoutes';
+import inventoryListRoutes from './routes/inventoryListRoutes'; 
+import inventoryTransactionRoutes from './routes/inventoryTransactionRoutes';
+import sellerRoutes from './routes/sellerRoutes';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,10 +27,37 @@ app.use('/api/brandlist', brandlistRoutes);
 app.use('/api/category-dimensions', categoryDimensionRoutes);
 app.use('/api/categorytypes', categoryTypesRoutes);
 
+// Routes
+app.use('/api/contact-us', contactusRoutes);
+
+// Routes
+app.use('/api/locations', locationsRoutes); // Register locations routes
+
+// product routes
+app.use('/api/products', productRoutes);
+
+
+// Use product dimension routes
+app.use('/productdimensions', productDimensionRoutes);
+
+
+// Inventory Routes
+app.use('/api/inventory', inventoryRoutes);
+
+
+// Define routes
+app.use('/inventorylist', inventoryListRoutes); // Use your inventory list routes
+
+// API Routes
+app.use('/api/inventory-transactions', inventoryTransactionRoutes);
+
+// Seller Routes
+app.use('/api/sellers', sellerRoutes);
+
 
 
 // Setup Swagger API documentation
-setupSwagger(app); // Initialize Swagger
+setupSwagger(app); 
 
 // Test database connection
 sequelize
